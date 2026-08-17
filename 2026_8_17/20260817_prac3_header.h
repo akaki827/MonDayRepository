be67695481e97c160f98836548684455671690b6
あかき <jagaimo287@gmail.com>
2026-08-17 12:18:47 +0900

    Delete 2026_8_17 directory

--- 2026_8_17/20260817_prac3_header.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-//’è”
-const int CARD_MAX = 28;
-
-void Card();
--- 2026_8_17/ソース.cpp
+++
@@ -1,9 +0,0 @@
-#include "20260817_prac3_header.h"
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-	Card();
-}
